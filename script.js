04791b985459b51a7a15e745ef066c8048822c8e
Md. Anisur Rahman <poetanis@gmail.com>
2025-07-30 18:04:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,7 @@ codeInput.addEventListener("input", syncHighlight);
 
 // Initial load
 window.onload = () => {
-  const sampleCode = `#include <stdio.h>
-
-// Bubble sort function
-void bubbleSort(int arr[], int n) {
+  const sampleCode = `void bubbleSort(int arr[], int n) {
   for (int i = 0; i < n - 1; i++) {
     for (int j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
@@ -32,25 +29,6 @@ void bubbleSort(int arr[], int n) {
       }
     }
   }
-}
-
-int main() {
-  int arr[] = {64, 34, 25, 12, 22, 11, 90};
-  int n = sizeof(arr) / sizeof(arr[0]);
-
-  printf("Original array: \n");
-  for (int i = 0; i < n; i++)
-    printf("%d ", arr[i]);
-  printf("\n");
-
-  bubbleSort(arr, n);
-
-  printf("Sorted array: \n");
-  for (int i = 0; i < n; i++)
-    printf("%d ", arr[i]);
-  printf("\n");
-
-  return 0;
 }`;
   codeInput.innerText = sampleCode;
   syncHighlight();
